6b98d077592ce9016924f38a111704aa03536695
Veronika Surkova <surkova_nika92@mail.ru>
2023-08-01 01:59:25 +0300

    add tests

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -4,6 +4,7 @@ import pytest
 
 def test_get():
     assert arrs.get([1, 2, 3], 1, "test") == 2
+    assert arrs.get([1, 2, 3], -1, "test") == "test"
 
 
 def test2_get():
@@ -14,3 +15,6 @@ def test2_get():
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([], ) == []
+    assert arrs.my_slice([1, 2, 3, 4], -3) == [2, 3, 4]
+    assert arrs.my_slice([1, 2, 3, 4], -5) == [1, 2, 3, 4]
