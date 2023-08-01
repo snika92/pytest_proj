09e91667ce477a8ec0fa792aaead108faa652764
Veronika Surkova <surkova_nika92@mail.ru>
2023-08-01 16:43:17 +0300

    optimize tests

--- tests/test_dicts.py
+++ tests/test_dicts.py
@@ -1,7 +1,14 @@
 from utils import dicts
+import pytest
 
 
 def test_get_val():
     assert dicts.get_val({"vcs": "mercurial"}, "vcs") == "mercurial"
-    assert dicts.get_val({}, "vcs", "git") == "git"
-    assert dicts.get_val({}, "vcs", "bazaar") == "bazaar"
+
+
+@pytest.mark.parametrize("collection, key, default, expected", [
+    ({}, "vcs", "git", "git"),
+    ({}, "vcs", "bazaar", "bazaar")
+])
+def test_get_val(collection, key, default, expected):
+    assert dicts.get_val(collection, key, default) == expected
